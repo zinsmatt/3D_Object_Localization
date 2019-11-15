e5dee9d97563620b2ba465e911533c423aa3c598
Zins Matthieu <matthieu.zins@inria.fr>
2019-11-15 16:33:33 +0100

    tools is working

--- tools.py
+++ tools.py
@@ -6,7 +6,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
+from scipy.spatial.transform import Rotation as R
+import cv2
 
 def bbox_to_ellipse(obb):
     """
@@ -58,7 +60,7 @@ def compute_B(P):
     p11, p12, p13, p14 = P[0, :]
     p21, p22, p23, p24 = P[1, :]
     p31, p32, p33, p34 = P[2, :]
-    B = np.array([[p11**2, 2*p12*p11, 2*p13*p11, 2*p14*p11, p12**2, 2*p13*p12, 2*p14*12, p13**2, 2*p13*p14, p14**2],
+    B = np.array([[p11**2, 2*p12*p11, 2*p13*p11, 2*p14*p11, p12**2, 2*p13*p12, 2*p14*p12, p13**2, 2*p13*p14, p14**2],
                   [p21*p11, p21*p12+p22*p11, p23*p11+p21*p13, p24*p11+p21*p14, p22*p12, p22*p13+p23*p12, p22*p14+p24*p12, p23*p13, p23*p14+p24*p13, p24*p14],
                   [p31*p11, p31*p12+p32*p11, p33*p11+p31*p13, p34*p11+p31*p14, p32*p12, p32*p13+p33*p12, p32*p14+p34*p12, p33*p13, p33*p14+p34*p13, p34*p14],
                   [p21**2, 2*p22*p21, 2*p23*p21, 2*p24*p21, p22**2, 2*p23*p22, 2*p24*p22, p23**2, 2*p23*p24, p24**2],
@@ -160,36 +162,44 @@ def reconstruct_objects(proj_matrices, ellipses, obj_appeareances, method, x_est
     nb_views = np.sum(obj_appeareances, 0)
     reconstructible_indices = np.where(nb_views > 2)[0]
 
+    quadrics = []
     for i in reconstructible_indices:
         visibility = obj_appeareances[:, i]
         n_views = np.sum(visibility)
-        selector = np.kron(visibility, [1, 1, 1].T)
+        print(visibility)
+        selector = np.kron(visibility, [1, 1, 1]).T.astype(bool)
+        print(selector.shape)
+        print(proj_matrices.shape)
         P = proj_matrices[selector, :]
-        C = ellipses[selector, i:i+3]
+        C = ellipses[selector, i*3:i*3+3]
         print(P.shape, C.shape)
 
         T = np.eye(4)
-        T[:3, 3] = x_est
+        T[:3, 3] = -x_est[:, i]
 
         P_new = np.zeros_like(P)
-        for vi in n_views:
+        for vi in range(n_views):
             P_new[vi*3:vi*3+3, :] = (T.T @  P[vi*3:vi*3+3, :].T).T
 
         if method == "SVD":
             Qadj = reconstruct_ellipsoid(P_new, C)
         else:
             print("Unknown method")
+        Qadj /= -Qadj[3, 3]
+        print(Qadj)
 
         Qadj = T @ Qadj @ T.T
         Qadj = 0.5 * (Qadj + Qadj.T)
         Qadj /= -Qadj[3, 3]
-        return Qadj
+        quadrics.append(Qadj)
+        
+    return quadrics
 
 
 def decompose_ellipse(C):
     """
-        Decompose an ellipse into 2 half-axis,
-        an orientation and a center. (returned in this order)
+        Decompose an ellipse into: half-axes, rotation and center
+        C: adjointe matrix of the ellipse (dual form)
     """
     if C[2, 2] > 0:
         C = -C / C[2, 2]
@@ -198,13 +208,10 @@ def decompose_ellipse(C):
                   [0.0, 1.0, -center[1]],
                   [0.0, 0.0, 1.0]])
     C_center = T @ C @ T.T
-    # force symetry
     C_center = 0.5 * (C_center + C_center.T)
-
-    D, V = np.linalg.eig(C_center[:2, :2])
+    D, R = np.linalg.eig(C_center[:2, :2])
     ax = np.sqrt(np.abs(D))
-
-    return ax, V, center
+    return ax, R, center
 
 def sym2vec(M):
     """
@@ -225,11 +232,12 @@ def vec2sym(v):
     while n < v.size:
         x += 1
         n += x
-    M = np.zeros((n, n), dtype=np.float)
+    M = np.zeros((x, x), dtype=np.float)
     a = 0
-    for i in range(n):
-        M[i:, i] = v[a:a+(n-i)]
-        a += (n-i)
+    for i in range(x):
+        M[i:, i] = v[a:a+(x-i)]
+        M[i, i:] = v[a:a+(x-i)]
+        a += (x-i)
     return M
 
 
@@ -245,32 +253,114 @@ def reconstruct_ellipsoid(Ps, Cs):
         C = Cs[i*3:i*3+3, :]
 
         ax, R, center = decompose_ellipse(C)
+#        print("ax center = ", ax, center)
 
         h = np.sqrt(np.sum(ax**2))
         H = np.array([[h, 0.0, center[0]],
                       [0.0, h, center[1]],
                       [0.0, 0.0, 1.0]])
+#        print("H = ", H)
         H_inv = np.linalg.inv(H)
-
+#        print(H_inv)
+        
         P = Ps[i*3:i*3+3, :]
         new_P = P.T @ H_inv.T
+        
+        
 
-        B = compute_B(new_P)
+        B = compute_B(new_P.T)
+        
 
         # normalize and center ellipsoid
-        C_nc = H @ C @ H.T
+        C_nc = H_inv @ C @ H_inv.T
         C_nc_vec = sym2vec(C_nc)
         C_nc_vec /= -C_nc_vec[-1]
 
-
         M[6*i:6*i+6, :10] = B
-        M[6*i:6*i+6, i] = -C_nc_vec
+        M[6*i:6*i+6, 10+i] = -C_nc_vec
+
 
     print("Reconstruction method: SVD")
     U, S, Vt = np.linalg.svd(M)
+#    print(S)
+#    print(U.shape)
+#    print(S.shape)
+#    print(Vt.shape)
+#    tmp = np.zeros((U.shape[1], Vt.shape[0]))
+#    tmp[:S.shape[0], :S.shape[0]] = np.diag(S)
+#    A = U @ tmp @ Vt
+#    print(np.sum(np.abs(A - M)))
+
     w = Vt[-1, :]
     Qadj_vec = w[:10]
     Qadj = vec2sym(Qadj_vec)
 
     return Qadj
+
+dataset_folder = "/home/mzins/Dataset/rgbd_dataset_freiburg2_desk/"
+gt_poses_file = os.path.join(dataset_folder, "groundtruth.txt")
+rgb_images_folder = os.path.join(dataset_folder, "rgb")
+
+
+detection_associations = "/home/mzins/dev/3D-Object-Localisation/ellipses_association/more_objects/maskrcnn.txt"
+images_to_use_file = "/home/mzins/dev/3D-Object-Localisation/ellipses_association/more_objects/maskrcnn.used_images.txt"
+
+C = np.loadtxt(detection_associations)
+K = np.array([[520.9, 0.0, 325.1],
+              [0.0, 521.0, 249.7],
+              [0.0, 0.0, 1.0]])
+poses = np.loadtxt(gt_poses_file)
+timestamps = poses[:, 0]
+positions = poses[:, 1:4]
+orientations = poses[:, 4:]
+images_to_use = np.genfromtxt(images_to_use_file, dtype=str)
+
+
+images = []
+projections = []
+for f in images_to_use:
+    images.append(cv2.imread(f))
+    
+    time = float(os.path.splitext(f)[0])
+    
+    d = np.abs(timestamps - time)
+    index = np.argmin(d)
+    
+    rotation = R.from_quat(orientations[index, :])
+    Rc_w = rotation.as_dcm().T
+    Tc_w = - Rc_w.dot(positions[index, :])
+    
+    Rt = np.hstack((Rc_w, Tc_w.reshape((-1, 1))))
+    projections.append(K @ Rt)
+projections = np.vstack(projections)
+    
+n_images = C.shape[0] // 3
+n_objects = C.shape[1] // 3
+print(n_images, " images")
+print(n_objects, " objects")
+
+to_use = np.zeros((n_images, n_objects), dtype=int)
+for i in range(n_images):
+    for j in range(n_objects):
+        if abs(C[i*3+2, j*3+2]) > 1e-3:
+            to_use[i, j] = 1
+            
+
+centers = np.zeros((3, n_objects), dtype=float)
+    
+quadrics = reconstruct_objects(projections, C, to_use, "SVD", centers)
+
+# update the estimated centers of the ellipsoids
+for i in range(n_objects):
+    Q = quadrics[i]
+    centers[:, i] = -Q[:3, -1]
+
+# recompute the quadrics with better approximation of the ellipsoids centers
+quadrics = reconstruct_objects(projections, C, to_use, "SVD", centers)
+
+
+
+    
+np.savetxt("ellipsoids_python.txt", np.vstack(quadrics))
+    
 
